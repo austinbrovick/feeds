5f8145e9dc78b064d4985e6b96a92b9e1217eaa2
Austin Brovick <austinbrovick@gmail.com>
2017-08-11 13:27:21 +0200

    added admin and the ability to add feeds

--- feeds/views.py
+++ feeds/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 from .forms import FeedForm
+from .models import Feed
 
 
 
@@ -11,7 +12,20 @@ def index(request):
 
 
 def create_feed(request):
+    # form = FeedForm(request.POST)
+    # if form.is_valid():
+        # try:
+        # feed = Feed.objects.get(name=form.cleaned_data['name']
+        # except Feed.DoesNotExist:
+        #     print("yup")
+        #     feed = Feed.objects.create(name=form.cleaned_data['name'])
+        #     feed.save()
     form = FeedForm(request.POST)
     if form.is_valid():
-        print("hello world")
-        return redirect('index')
+        try:
+            feed = Feed.objects.get(name=form.cleaned_data['name'])
+        except Feed.DoesNotExist:
+            feed = Feed.objects.create(name=form.cleaned_data['name'])
+            feed.save()
+            print("saved {}".format(feed))
+    return redirect('index')
